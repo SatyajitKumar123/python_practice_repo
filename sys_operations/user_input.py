5c5b95c86c23962262f8e94c375da4800e44c958
Satyajit Kumar <satyajitkumar2472@gmail.com>
2025-09-30 19:16:10 +0530

    user_input.py

--- sys_operations/user_input.py
+++ sys_operations/user_input.py
@@ -1,15 +1,19 @@
 import sys
+import os
+
+## __file__ gives the full path to the current script file
+current_script_path = __file__
+
+## os.path.basename() extracts the filename from the full path
+current_script_filename = os.path.basename(current_script_path)
 
 ## Check if enough arguments are provided
 if len(sys.argv) < 4:
-    print("Usage: python main.py name age address")
+    print(f"Usage: Python {current_script_filename} name age address")
     sys.exit(1)
 
 name = sys.argv[1]
 age = int(sys.argv[2])
 address = sys.argv[3]
 
-info = {"name": name, "age": age, "address": address}
-
-for key, value in info.items():
-    print(f"{key}: {value}")
+print(f"You are {name}, {age} years old. \nYou lives in {address}.")
